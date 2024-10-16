5eb6eb643dc952bc84c14c5fe7f3a1949fb64b43
Peter Windemuller <p.windemuller@cloudrepublic.nl>
2024-10-16 19:13:15 +0200

    Add project files.

---
+++ Test_LogzIO/Program.cs
@@ -0,0 +1,45 @@
+﻿using Serilog;
+using Serilog.Sinks.Logz.Io;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.LogzIoDurableHttp(
+                    "https://listener-eu.logz.io:8071/?type=PeterLogzIoTest&token=sometoken",
+                    logzioTextFormatterOptions: new LogzioTextFormatterOptions
+                    {
+                        BoostProperties = true,
+                        LowercaseLevel = true,
+                        IncludeMessageTemplate = true,
+                        FieldNaming = LogzIoTextFormatterFieldNaming.CamelCase,
+                        EventSizeLimitBytes = 261120,
+                    })
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("Application", "Mijn Test applicatie")
+                .Enrich.WithProperty("Environment", "Test")
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                logger.Information("Test {testwaarde}", "Een waarde");
+                logger.Information("Test {getalwaarde}", i);
+                // gives the log enough time to be sent to Logz.io
+                Console.WriteLine("Volgende");
+            }
+            Thread.Sleep(5000);
+
+
+            Console.WriteLine("Klaar");
+            Console.ReadLine();
+        }
+
+    }
+}
